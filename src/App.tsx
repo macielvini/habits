1f4e5c00b6501a921d2708417306c83bd7338f92
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-20 17:47:23 -0300

    feat: days table component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./styles/global.css";
 
 import { useState } from "react";
 import { Header } from "./components/Header";
+import { HabitsTable } from "./components/HabitsTable";
 
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-background text-white w-screen h-screen flex justify-center items-center">
+    <div className="bg-background text-white font-bold w-screen h-screen flex justify-center items-center flex-col">
       <Header />
+      <HabitsTable />
     </div>
   );
 }
---
+++ src/components/HabitsTable.tsx
@@ -0,0 +1,38 @@
+import { generateDaysOfYear } from "../utils/generateDaysOfYear";
+import { TableDay } from "./TableDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const daysOfYear = generateDaysOfYear();
+
+export function HabitsTable() {
+  const minimumTableDays = 18 * 7;
+  const daysToFill = Math.abs(daysOfYear.length - minimumTableDays);
+
+  return (
+    <div className="w-full max-w-5xl flex ">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((d, i) => (
+          <div
+            key={i}
+            className="text-zinc-400 h-10 w-10 flex items-center justify-center"
+          >
+            {d}
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {daysOfYear.map((d, i) => (
+          <TableDay key={i} />
+        ))}
+        {daysToFill > 0 &&
+          Array.from({ length: daysToFill }).map((_, i) => (
+            <div
+              key={i}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-900 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
+      </div>
+    </div>
+  );
+}

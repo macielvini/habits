ed8a4d00f2ed529ac024ecd3a74b467fc98c76a4
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-02-23 15:52:14 -0300

    feat: habit popover checklist

--- src/components/HabitPopover.tsx
+++ src/components/HabitPopover.tsx
@@ -1,5 +1,7 @@
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { ProgressBar } from "./ProgressBar";
+import { Check } from "phosphor-react";
 
 interface HabitProps {
   weekDay: string;
@@ -17,6 +19,20 @@ export function HabitPopover({ weekDay, date, progress }: HabitProps) {
           {date}
         </span>
         <ProgressBar progress={progress} />
+
+        <div className="mt-6 flex flex-col gap3">
+          <Checkbox.Root className="flex items-center gap-3 group  ">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400 group-">
+              2L de água
+            </span>
+          </Checkbox.Root>
+        </div>
       </Popover.Content>
     </Popover.Portal>
   );
